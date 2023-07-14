7e73a78f132d78a03a22afea9f0e31f30c5de547
Edgarmu <100827160+Edgarmu@users.noreply.github.com>
2023-07-14 15:02:52 +0300

    error all

---
+++ pkg/handler/handler.go
@@ -0,0 +1,36 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+type Handler struct {
+}
+
+func (h *Handler) InitRoutes() *gin.Engine {
+	router := gin.New()
+	auth := router.Group("/auth")
+	{
+		auth.POST("/sign-up", h.singUp)
+		auth.POST("/sign-in", h.singIn)
+	}
+	api := router.Group("api")
+	{
+		list := api.Group("/list")
+		{
+			list.POST("/", h.createList)
+			list.GET("/", h.getAllList)
+			list.GET("/:id", h.getListById)
+			list.PUT("/:id", h.updateList)
+			list.DELETE("/id", h.deleteList)
+
+			item := list.Group(":id/items")
+			{
+				item.POST("/", h.createItem)
+				item.GET("/", h.getAllItem)
+				item.GET("/:item_id", h.getItemById)
+				item.PUT("/:item_id", h.updateItem)
+				item.DELETE("/:item_id", h.deleteItem)
+			}
+		}
+	}
+	return router
+}
